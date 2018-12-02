b665b6d7ad480d28dab917688bb3291dcff43156
KaloyanDanev <kaloian4obg99@gmail.com>
2018-12-02 19:12:16 +0200

    Structure of the project created. Added Bootstrap and navigation bar.

---
+++ src/FitnessGuru.Models/FitnessGuruWebUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FitnessGuru.Models
+{
+    // Add profile data for application users by adding properties to the FitnessGuruWebUser class
+    public class FitnessGuruWebUser : IdentityUser
+    {
+    }
+}
